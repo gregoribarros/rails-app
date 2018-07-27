532b380d0f481510239904a686d0086f4db1407b
Grégori Barros <gregorifranciscobarros@gmail.com>
2018-07-27 12:32:39 -0300

    finalizacao

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,15 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
 var cores = ["#481E9D","#8D74C0","#CA4B57","#2B2D42","#1C77C3","#81B29A","#E98039"];
-var mudou = false;
+var fundoAlterado = false;
+
 function getRandom(max) {
     return Math.floor(Math.random() * max )
 }
 
-function acha(id) 
+function alteraMensagem(id) 
 {
 	texto = document.getElementById(id).innerHTML;
 
@@ -28,7 +17,7 @@ function acha(id)
 	document.getElementById(id).innerHTML=txt;
 	if (txt != texto)
 	{
-	document.getElementById(id*13).style.color= cores[getRandom(6)];
+	document.getElementById(id*13).style.color = cores[getRandom(6)];
 	}
 }
 
@@ -40,17 +29,17 @@ function inicializa(){
 function testa_form(){
 	texto = document.getElementById("meu-input").value;
 
-	if(event.keyCode == 70 && mudou){
+	if(event.keyCode == 70 && !fundoAlterado){
 		if (texto.toLowerCase().indexOf("mconf") != -1){
 	    	document.body.style.backgroundColor = cores[getRandom(6)];
-	    	mudou = true;
+	    	fundoAlterado = true;
 		} 
 	}
 	else if (event.keyCode == 8){
 		if(texto.toLowerCase().indexOf("mconf") == -1){
 
 			document.body.style.backgroundColor = "white";
-			mudou = false;
+			fundoAlterado = false;
 		}
 
 	}
